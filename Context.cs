e57db88b7cb305ba95ebc02738c64b16d483a0ad
BEA <124007083+BrayanEAA@users.noreply.github.com>
2023-10-15 21:50:49 -0600

    fin

---
+++ Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+public class Context : DbContext
+{
+    public DbSet<Student> estudiante { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-J91J8C7;Database=Progra2; Trusted_Connection = SSPI; MultipleActiveResultSets = true; Trust Server Certificate = true; ");
+    }
+}
+
